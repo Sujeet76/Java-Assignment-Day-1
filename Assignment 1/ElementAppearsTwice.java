0e97d916750beb0bb4138f663695d798235b7b21
Sujeet <ksujeetkumar7678@gmail.com>
2023-06-25 00:19:22 +0530

    Adding Assignment 1

--- Assignment 1/ElementAppearsTwice.java
+++ Assignment 1/ElementAppearsTwice.java
@@ -5,6 +5,7 @@ class ElementAppearsTwice {
   }
 
   // function
+  // function which checks the duplicate
   public static Boolean appearsAtLeastTwice(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
       for (int j = i + 1; j < arr.length; j++) {
